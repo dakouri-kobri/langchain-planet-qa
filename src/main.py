817d6a9717d8b25cf2d9ee4565c6d845fcec325b
Dakouri Kobri <dakouri.m.kobri@gmail.com>
2026-03-29 20:19:51 +0200

    refactor:structure main script with modular sections and helper functions

--- src/main.py
+++ src/main.py
@@ -1,30 +1,45 @@
+# Imports =======================================
+
 from langchain_core.prompts import PromptTemplate
 from langchain_groq import ChatGroq
 import dotenv
 
 
-def main():
+# Configuration / Setup =========================
+
+def setup_llm():
     dotenv.load_dotenv()
 
-    llm = ChatGroq(
+    return ChatGroq(
         model="llama-3.3-70b-versatile",
         temperature=0.5,
-        max_retries=2,
+        max_retries=2
     )
 
-    question = input("Ask something: ")
 
+# Prompt definition =============================
+
+def build_prompt(question:str):
     template = PromptTemplate.from_template(
-        "You are a helpful assistant who answers user questions. "
-        "You are asked: {question}"
+        "You are a helpful assistant who answers questions users may have. "
+        "You are asked: {question}."
     )
 
-    prompt = template.invoke({"question": question})
+    return template.invoke({"question": question})
 
+
+# Main Application Logic ========================
+
+def main():
+    llm = setup_llm()
+    question = input("What is your question? ")
+    prompt = build_prompt(question)
     response = llm.invoke(prompt)
 
     print(response.content)
 
+
+# Entry Point ===================================
 
 if __name__ == "__main__":
     main()
