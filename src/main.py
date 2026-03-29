80b35a7a8a24b49a8f0d9069454961d14784ddf9
Dakouri Kobri <dakouri.m.kobri@gmail.com>
2026-03-29 21:14:10 +0200

    feat: implement few-shot prompting for structured planet responses

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 # Imports =======================================
+from http.client import responses
 
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate, FewShotPromptTemplate
 from langchain_groq import ChatGroq
 import dotenv
 
@@ -17,25 +18,70 @@ def setup_llm():
     )
 
 
-# Prompt definition =============================
+# Few-shot Examples =============================
 
-def build_prompt(question:str):
-    template = PromptTemplate.from_template(
-        "You are a helpful assistant who answers questions users may have. "
-        "You are asked: {question}."
+def get_examples():
+    examples = [
+        {
+            "input": "Jupiter",
+            "output": (
+                "Jupiter is the largest planet in the solar system.\n"
+                "It is a gas giant primarily composed of hydrogen and helium.\n"
+                "It has a Great Red Spot, a massive storm, and at least 79 known moons."
+            )
+        },
+        {
+            "input": "Mars",
+            "output": (
+                "Mars is the fourth planet from the Sun.\n"
+                "It has a thin atmosphere composed mainly of carbon dioxide.\n"
+                "Notable features include Olympus Mons and Valles Marineris."
+            )
+        }
+    ]
+
+    return examples
+
+
+# Prompt builder =============================
+
+def build_prompt():
+    examples = get_examples()
+    example_template = PromptTemplate.from_template(
+        "Planet: {input}\nAnswer:\n{output}\n",
     )
 
-    return template.invoke({"question": question})
+    few_shot_prompt = FewShotPromptTemplate(
+        examples=examples,
+        example_prompt=example_template,
+        suffix = (
+            "Planet: {question}\n"
+            "Answer:\n"
+            "Provide key details including:\n"
+            "- Physical characteristics (size, composition, atmosphere)\n"
+            "- Notable features (rings, moons, surface conditions\n"
+            "- Scientific or historical significance\n"
+            "- Fun or surprising facts\n"
+        ),
+        input_variables=["question"]
+    )
+
+    return few_shot_prompt
 
 
 # Main Application Logic ========================
 
 def main():
     llm = setup_llm()
-    question = input("What is your question? ")
-    prompt = build_prompt(question)
-    response = llm.invoke(prompt)
+    prompt_template = build_prompt()
 
+    question = input("Enter a planet name: ")
+
+    final_prompt = prompt_template.format(question=question)
+
+    response = llm.invoke(final_prompt)
+
+    print("========= Answer =========\n")
     print(response.content)
 
 
